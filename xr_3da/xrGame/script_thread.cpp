1ba37b0bc6811314cc36f12b3524dd4eb616d0e3
Skajdrovski <cssteam1337pl@gmail.com>
2025-09-26 12:45:30 +0200

    Updated LuaJIT from 1.1.8 to 2.1.0

--- xr_3da/xrGame/script_thread.cpp
+++ xr_3da/xrGame/script_thread.cpp
@@ -10,7 +10,7 @@
 #include "script_space.h"
 extern "C" {
 	#include "lua.h"
-	#include "lstate.h"
+	#include "lj_state.h"
 	#include "luajit.h"
 };
 #include "script_engine.h"
@@ -61,8 +61,7 @@ CScriptThread::CScriptThread(LPCSTR caNamespaceName, bool do_string, bool reload
 		}
 
 //		print_stack_		(ai().script_engine().lua());
-//		m_virtual_machine	= lua_newthread(ai().script_engine().lua());
-		m_virtual_machine	= lua_newcthread(ai().script_engine().lua(),0);
+		m_virtual_machine	= lua_newthread(ai().script_engine().lua());
 		VERIFY2				(lua(),"Cannot create new Lua thread");
 //		print_stack_		(ai().script_engine().lua());
 //		m_thread_reference	= luaL_ref(ai().script_engine().lua(),LUA_REGISTRYINDEX);
