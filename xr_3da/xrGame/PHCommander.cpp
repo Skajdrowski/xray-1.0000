1f610ea18cba52974f109706ab0133469f699f15
Skajdrovski <cssteam1337pl@gmail.com>
2025-10-26 02:44:33 +0200

    OGSR level:set_pause

--- xr_3da/xrGame/PHCommander.cpp
+++ xr_3da/xrGame/PHCommander.cpp
@@ -6,6 +6,7 @@ CPHCall::CPHCall(CPHCondition* condition,CPHAction* action)
 {
 	m_action=action;
 	m_condition=condition;
+	paused=0;
 }
 
 CPHCall::~CPHCall()
@@ -31,6 +32,9 @@ bool CPHCall::is_any(CPHReqComparerV* v)
 {
 	return m_action->compare(v)||m_condition->compare(v);
 }
+
+void CPHCall::setPause(u32 ms) { paused = Device.dwTimeGlobal + ms; }
+bool CPHCall::isPaused() { return paused > Device.dwTimeGlobal; }
 
 void delete_call(CPHCall* &call)
 {
@@ -64,6 +68,8 @@ void CPHCommander::update	()
 {
 	for(u32 i=0; i<m_calls.size(); i++)
 	{
+		if (m_calls[i]->isPaused())
+			continue;
 		try
 		{
 			m_calls[i]->check();
@@ -84,13 +90,14 @@ void CPHCommander::update	()
 	}
 }
 
-void CPHCommander::add_call(CPHCondition* condition,CPHAction* action,PHCALL_STORAGE& cs)
+CPHCall* CPHCommander::add_call(CPHCondition* condition,CPHAction* action,PHCALL_STORAGE& cs)
 {
 	cs.push_back(xr_new<CPHCall>(condition,action));
+	return cs.back();
 }
-void CPHCommander::add_call(CPHCondition* condition,CPHAction* action)
+CPHCall* CPHCommander::add_call(CPHCondition* condition,CPHAction* action)
 {
-	add_call(condition,action,m_calls);
+	return add_call(condition,action,m_calls);
 	
 }
 void CPHCommander::remove_call(PHCALL_I i,PHCALL_STORAGE& cs)
@@ -159,16 +166,22 @@ void CPHCommander::remove_call(CPHReqComparerV* cmp_condition,CPHReqComparerV* c
 	remove_call(cmp_condition,cmp_action,m_calls);
 }
 
-void CPHCommander::add_call_unique(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action,PHCALL_STORAGE& cs)
+CPHCall* CPHCommander::add_call_unique(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action,PHCALL_STORAGE& cs)
 {
 	if(cs.end()==find_call(cmp_condition,cmp_action,cs))
 	{
-		add_call(condition,action,cs);
+		return add_call(condition,action,cs);
+	}
+	else
+	{
+		xr_delete(condition);
+		xr_delete(action);
+		return NULL;
 	}
 }
-void CPHCommander::add_call_unique(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action)
+CPHCall* CPHCommander::add_call_unique(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action)
 {
-	add_call_unique(condition,cmp_condition,action,cmp_action,m_calls);
+	return add_call_unique(condition,cmp_condition,action,cmp_action,m_calls);
 }
 struct SRemoveRped
 {
@@ -198,13 +211,13 @@ void CPHCommander::remove_calls(CPHReqComparerV* cmp_object)
 	remove_calls(cmp_object,m_calls);
 }
 
-void		CPHCommander::		add_call_unique_as				(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action)
+CPHCall*		CPHCommander::		add_call_unique_as				(CPHCondition* condition,CPHReqComparerV* cmp_condition,CPHAction* action,CPHReqComparerV* cmp_action)
 {
-	add_call_unique(condition,cmp_condition,action,cmp_action,m_calls_as_add_buffer);
+	return add_call_unique(condition,cmp_condition,action,cmp_action,m_calls_as_add_buffer);
 }
-void		CPHCommander::		add_call_as					(CPHCondition* condition,CPHAction* action)
+CPHCall*		CPHCommander::		add_call_as					(CPHCondition* condition,CPHAction* action)
 {
-	add_call(condition,action,m_calls_as_add_buffer);
+	return add_call(condition,action,m_calls_as_add_buffer);
 }
 
 
--- xr_3da/xrGame/level_script.cpp
+++ xr_3da/xrGame/level_script.cpp
@@ -290,13 +290,13 @@ bool is_level_present()
 	return (!!g_pGameLevel);
 }
 
-void add_call(const luabind::functor<bool> &condition,const luabind::functor<void> &action)
+CPHCall* add_call(const luabind::functor<bool> &condition,const luabind::functor<void> &action)
 {
 	luabind::functor<bool>		_condition = condition;
 	luabind::functor<void>		_action = action;
 	CPHScriptCondition	* c=xr_new<CPHScriptCondition>(_condition);
 	CPHScriptAction		* a=xr_new<CPHScriptAction>(_action);
-	Level().ph_commander_scripts().add_call(c,a);
+	return Level().ph_commander_scripts().add_call(c,a);
 }
 
 void remove_call(const luabind::functor<bool> &condition,const luabind::functor<void> &action)
@@ -306,7 +306,7 @@ void remove_call(const luabind::functor<bool> &condition,const luabind::functor<
 	Level().ph_commander_scripts().remove_call(&c,&a);
 }
 
-void add_call(const luabind::object &lua_object, LPCSTR condition,LPCSTR action)
+CPHCall* add_call(const luabind::object &lua_object, LPCSTR condition,LPCSTR action)
 {
 //	try{	
 //		CPHScriptObjectCondition	*c=xr_new<CPHScriptObjectCondition>(lua_object,condition);
@@ -315,7 +315,7 @@ void add_call(const luabind::object &lua_object, LPCSTR condition,LPCSTR action)
 		luabind::functor<void>		_action = object_cast<luabind::functor<void> >(lua_object[action]);
 		CPHScriptObjectConditionN	*c=xr_new<CPHScriptObjectConditionN>(lua_object,_condition);
 		CPHScriptObjectActionN		*a=xr_new<CPHScriptObjectActionN>(lua_object,_action);
-		Level().ph_commander_scripts().add_call_unique(c,c,a,a);
+		return Level().ph_commander_scripts().add_call_unique(c,c,a,a);
 //	}
 //	catch(...)
 //	{
@@ -330,12 +330,12 @@ void remove_call(const luabind::object &lua_object, LPCSTR condition,LPCSTR acti
 	Level().ph_commander_scripts().remove_call(&c,&a);
 }
 
-void add_call(const luabind::object &lua_object, const luabind::functor<bool> &condition,const luabind::functor<void> &action)
+CPHCall* add_call(const luabind::object &lua_object, const luabind::functor<bool> &condition,const luabind::functor<void> &action)
 {
 
 	CPHScriptObjectConditionN	*c=xr_new<CPHScriptObjectConditionN>(lua_object,condition);
 	CPHScriptObjectActionN		*a=xr_new<CPHScriptObjectActionN>(lua_object,action);
-	Level().ph_commander_scripts().add_call(c,a);
+	return Level().ph_commander_scripts().add_call(c,a);
 }
 
 void remove_call(const luabind::object &lua_object, const luabind::functor<bool> &condition,const luabind::functor<void> &action)
@@ -588,15 +588,17 @@ void CLevel::script_register(lua_State *L)
 		def("map_has_object_spot",				map_has_object_spot),
 		def("map_change_spot_hint",				map_change_spot_hint),
 
+		class_<CPHCall>("CPHCall").def("set_pause", &CPHCall::setPause),
+
 		def("start_stop_menu",					start_stop_menu),
 		def("add_dialog_to_render",				add_dialog_to_render),
 		def("remove_dialog_to_render",			remove_dialog_to_render),
 		def("main_input_receiver",				main_input_receiver),
 		def("hide_indicators",					hide_indicators),
 		def("show_indicators",					show_indicators),
-		def("add_call",							((void (*) (const luabind::functor<bool> &,const luabind::functor<void> &)) &add_call)),
-		def("add_call",							((void (*) (const luabind::object &,const luabind::functor<bool> &,const luabind::functor<void> &)) &add_call)),
-		def("add_call",							((void (*) (const luabind::object &, LPCSTR, LPCSTR)) &add_call)),
+		def("add_call",							((CPHCall* (*) (const luabind::functor<bool> &,const luabind::functor<void> &)) &add_call)),
+		def("add_call",							((CPHCall* (*) (const luabind::object &,const luabind::functor<bool> &,const luabind::functor<void> &)) &add_call)),
+		def("add_call",							((CPHCall* (*) (const luabind::object &, LPCSTR, LPCSTR)) &add_call)),
 		def("remove_call",						((void (*) (const luabind::functor<bool> &,const luabind::functor<void> &)) &remove_call)),
 		def("remove_call",						((void (*) (const luabind::object &,const luabind::functor<bool> &,const luabind::functor<void> &)) &remove_call)),
 		def("remove_call",						((void (*) (const luabind::object &, LPCSTR, LPCSTR)) &remove_call)),
